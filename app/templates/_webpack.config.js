4e398c4ce2a826091bc2a2580670df2df4a0eefa
Nils Butenschön <butenschoen@bdrops.de>
2017-07-12 16:58:48 +0200

    Fix that webpack task for good

--- app/templates/_webpack.config.js
+++ app/templates/_webpack.config.js
@@ -1,15 +1,9 @@
 module.exports = {
     entry: {
-      app: ['./web-src/es6/app.js']<% if (useJQuery || useBootstrap) { %>,
-      libs: ['./web-src/js/dist/libs.all.js']
-      <% } %>
+      app: ['./web-src/es6/app.js'<% if (useJQuery || useBootstrap) { %>, './web-src/js/dist/libs.all.js'<% } %>]
     },
     output: {
-      <% if (useJQuery || useBootstrap) { %>
-        path: '/web/js'
-      <% } else { %>
-        filename: 'bundle.js'
-      <% } %>
+      filename: 'bundle.js'
     },
     module: {
       rules: [
